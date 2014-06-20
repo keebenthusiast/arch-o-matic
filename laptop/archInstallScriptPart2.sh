fdfb433a73d429166b1ada777d4450ab6d129b2a
CSCoder4ever <nxtpuny@gmail.com>
2014-06-20 13:27:54 -0700

    Update archInstallScriptPart2.sh

--- laptop/archInstallScriptPart2.sh
+++ laptop/archInstallScriptPart2.sh
@@ -44,12 +44,12 @@ if [ "$SyuR" = "y" ]; then
 	pacman -Syyu
 	sleep 1 && echo "Installing necessary desktop programs" # Optimized for a laptop with Intel Graphics.
         pacman -S wpa_supplicant dialog git alsa-utils alsa-plugins xorg-server xorg-xinit xorg-server-utils\
-        ttf-dejavu mesa arandr xf86-video-intel xf86-input-synaptics i3 sakura nitrogen Dmenu networkmanager\
-        xfce4-notifyd networkmanager-applet gnome-icon-theme wicd wicd-gtk wireless_tools gnome-keyring\
+        ttf-dejavu mesa arandr xf86-video-intel xf86-input-synaptics i3 sakura nitrogen dmenu networkmanager\
+        xfce4-notifyd network-manager-applet gnome-icon-theme wicd wicd-gtk wireless_tools gnome-keyring\
         thunar thunar-volman gvfs gvfs-afc tilda conky firefox htop pavucontrol xfce4-power-manager audacious\
         vlc terminator leafpad pulseaudio screenfetch scrot gimp lxappearance viewnior thunderbird libreoffice\
         flashplugin volumeicon fakeroot jshon expac wget gparted clipit transmission-gtk file-roller p7zip zip\
-        unrar arj unace lib32-glibc emacs mc openssh nfs-utils dzen2 remmina freerdp ntfs-3g ntfs-config gksu\
+        unrar arj unace lib32-glibc emacs mc openssh nfs-utils dzen2 remmina freerdp ntfs-3g gksu\
         lxde-icon-theme moc wavpack xfce4-screenshooter tmux timidity++ libstdc++5 linux-headers dosfstools
 	sleep 1 && read -p "would you like to install packer? y/n: " packerResponse
 	if [ "$packerResponse" = "y" ]; then
